83f5c45a3979863a47ef0af3514519be6c92e0e8
Anselm Schaefer <schaefab@basfad.basf.net>
2022-10-13 23:59:52 +0200

    add Eureka logic

--- src/main/java/com/anselm/movieinfoservice/MovieInfoServiceApplication.java
+++ src/main/java/com/anselm/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,8 +2,10 @@ package com.anselm.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //not mandatory anymore => adding to classpath is enough => because Eureka Registry is running on default port: it can find it
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {

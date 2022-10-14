02d73cbac4a4058b4a50b9ba1f42dde7b413c3a7
Anselm Schaefer <schaefab@basfad.basf.net>
2022-10-14 23:37:21 +0200

    add themoviedb service

--- src/main/java/com/anselm/movieinfoservice/MovieInfoServiceApplication.java
+++ src/main/java/com/anselm/movieinfoservice/MovieInfoServiceApplication.java
@@ -3,10 +3,17 @@ package com.anselm.movieinfoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient //not mandatory anymore => adding to classpath is enough => because Eureka Registry is running on default port: it can find it
 public class MovieInfoServiceApplication {
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
---
+++ src/main/java/com/anselm/movieinfoservice/resources/MovieSummary.java
@@ -0,0 +1,17 @@
+package com.anselm.movieinfoservice.resources;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieSummary {
+
+	  private String id;
+	  private String title;
+	  private String overview;
+}
